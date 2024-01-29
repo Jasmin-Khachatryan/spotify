3caa80341fd4f1963a2ac2dc1c6180f57b13bc93
Jasmin-Khachatryan <jaskhachatryan68@gmail.com>
2024-01-29 18:28:28 +0400

    password reset

--- spotify/urls.py
+++ spotify/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -10,6 +11,16 @@ urlpatterns = [
     path("music/", include("music.urls", namespace="music")),
     path("artist/", include("artist.urls", namespace="artist")),
     path("premium/", include("payment.urls", namespace="payments")),
+
+    path("change-password/", auth_views.PasswordChangeView.as_view()),
+    path("reset-password/", auth_views.PasswordResetView.as_view(), name="reset_password"),
+    path("password-reset-done/", auth_views.PasswordResetDoneView.as_view(),
+         name="password_reset_done"),
+    path("password-reset-confirm/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(),
+         name="password_reset_confirm"),
+    path("password-reset-complete/", auth_views.PasswordResetCompleteView.as_view(),
+         name="password_reset_complete"),
+
     path("__debug__/", include("debug_toolbar.urls")),
 
 ]
